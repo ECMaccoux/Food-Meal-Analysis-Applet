37edeb5997899038ac8347f8eb902743f4d1bd5d
vickblanke <tblanke2@wisc.edu>
2018-11-28 18:04:28 -0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -83,7 +83,7 @@ public class Main extends Application {
 			ScrollPane foodList = new ScrollPane();
 			Button temp = new Button();
 			for(int i=0; i<200; i++) {
-				temp=new Button("Test Food #" + i + " Test Food lmao");
+				temp=new Button("Test Food #" + i + " Test Food");
 				GUI.initScrollButton(temp);
 				foodPane.getChildren().add(temp);
 			}
@@ -91,7 +91,7 @@ public class Main extends Application {
 			VBox mealPane = new VBox();
 			ScrollPane mealList = new ScrollPane();
 			for(int i=0; i<200; i++) {
-				temp=new Button("Test Meal #" + i + " Test Meal lmao");
+				temp=new Button("Test Meal #" + i + " Test Meal");
 				GUI.initScrollButton(temp);
 				mealPane.getChildren().add(temp);
 			}
