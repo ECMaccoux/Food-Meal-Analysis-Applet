0faa890dab69534c4e525437e0f7f62b5f273570
ECMaccoux <emaccoux@wisc.edu>
2018-12-06 16:45:10 -0600

    Query Screen Needs
    Added file with requirements for Query screen

--- FoodData.java
+++ FoodData.java
@@ -2,8 +2,6 @@ package application;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileWriter;
-import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.HashMap;
